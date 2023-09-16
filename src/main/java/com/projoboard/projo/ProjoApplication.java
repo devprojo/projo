b60c0854df0f9452d4481fe28573366db0772125
Ante Balaic <ante@antebm.xyz>
2023-09-16 14:30:38 +0200

    initial project setup

---
+++ src/main/java/com/projoboard/projo/ProjoApplication.java
@@ -0,0 +1,13 @@
+package com.projoboard.projo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projoboard/projo/ProjoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projoboard.projo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
