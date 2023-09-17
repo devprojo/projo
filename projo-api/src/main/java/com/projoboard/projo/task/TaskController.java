1c1b4ec2ae74361fd7412ffba6e5943db2f459d5
Ante Balaic <ante@antebm.xyz>
2023-09-17 17:11:30 +0200

    Implement Task Repository and Controller
    Implement Task Repository, Service and Controller
    Integrate SwaggerUI and OpenAPI
    Remove thymeleaf dependency
    
    closes #15, closes #21

---
+++ projo-api/src/main/java/com/projoboard/projo/task/TaskController.java
@@ -0,0 +1,72 @@
+package com.projoboard.projo.task;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Implements tasks rest controller
+ */
+@RequestMapping("/tasks")
+@RestController
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    /**
+     * Returns all tasks.
+     *
+     * @return List<Task> - list of all task entries
+     */
+    @GetMapping("/")
+    public List<Task> getTasks() {
+        return taskService.getTasks();
+    }
+
+    /**
+     * Fetches task by id
+     *
+     * @param id
+     * @return task object
+     */
+    @GetMapping("/{id}")
+    public Task getTask(@PathVariable Long id) {
+        return taskService.getTask(id).orElse(null);
+    }
+
+    /**
+     * Creates a new task entry
+     *
+     * @param task
+     * @return Task - created task
+     */
+    @PostMapping("/")
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    /**
+     * Deletes task by ids
+     *
+     * @param id - task id
+     */
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+
+    /**
+     * Updates task
+     *
+     * @param task - updated tak data
+     * @param id   - id of the task to update
+     */
+    @PutMapping("/{id}")
+    public Task updateTask(@RequestBody Task task, @PathVariable Long id) {
+        return taskService.updateTask(task, id);
+    }
+}
