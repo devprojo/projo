255ec08374894d7f318d2ae96f8034930c7346b7
Lovre Gusar <lovreg79@gmail.com>
2023-09-18 09:27:45 +0200

    Implement controller and repository for Board
    implement controller, repository and service layer for Board
    closes #16

---
+++ projo-api/src/main/java/com/projoboard/projo/board/BoardService.java
@@ -0,0 +1,82 @@
+package com.projoboard.projo.board;
+
+import com.projoboard.projo.task.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Service layer for Board
+ */
+@Service
+public class BoardService {
+    private final BoardRepository boardRepository;
+    private final TaskRepository taskRepository;
+
+    public BoardService(BoardRepository boardRepository, TaskRepository taskRepository) {
+        this.boardRepository = boardRepository;
+        this.taskRepository = taskRepository;
+    }
+
+    /**
+     * Find all Board entries
+     *
+     * @return list of all Board entries
+     */
+    public List<Board> getBoards() {
+        return boardRepository.findAll();
+    }
+
+    /**
+     * Find a Board entry with a specified ID
+     *
+     * @param id id of Board to find
+     * @return Board object if found, else null
+     */
+    public Board getBoard(Long id) {
+        return boardRepository.findById(id).orElse(null);
+    }
+
+    /**
+     * Create a new Board entry
+     *
+     * @param board Board object to save in the repository
+     * @return created Board object, this object will have a new ID
+     */
+    public Board createBoard(Board board) {
+        taskRepository.saveAll(board.getTasks());
+        return boardRepository.save(board);
+    }
+
+    /**
+     * Update Board entry in the repository, if the entry with the specified ID doesn't exist
+     * a new entry will be created
+     *
+     * @param id       ID of a Board to update
+     * @param newBoard Board object with different fields
+     * @return updated Board object
+     */
+    public Board updateBoard(Long id, Board newBoard) {
+        Board board = boardRepository.findById(id).orElse(null);
+        if (board == null) {
+            newBoard.setId(id);
+            taskRepository.saveAll(newBoard.getTasks());
+            return boardRepository.save(newBoard);
+        } else {
+            board.setTitle(newBoard.getTitle());
+            board.getTasks().clear();
+            taskRepository.saveAll(newBoard.getTasks());
+            board.getTasks().addAll(newBoard.getTasks());
+            return boardRepository.save(board);
+        }
+    }
+
+    /**
+     * Delete Board entry with specified ID
+     *
+     * @param id ID of Board to delete
+     */
+    public void deleteBoard(Long id) {
+        boardRepository.deleteById(id);
+    }
+}
